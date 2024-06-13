a428b9c85ec43a46fcffc5f1b1699dc68ca64a99
Allan <168604543+allanw01@users.noreply.github.com>
2024-06-13 14:56:44 +1200

    Setup 'components' and 'screens' folder under app folder

---
+++ manaMathQuizApp/app/components/Quiz.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View,Text, StyleSheet, Touchable, TouchableOpacity } from "react-native";
+
+const Quiz=(props) => {
+    return (
+      <View style={styles.container}>
+    
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default Quiz;
